53606057c64dced4d8d8d28ac922102b03f9c2fc
SocWoo <kwkps21@gmail.com>
2016-03-17 12:18:24 +0900

    스위치를 통한 색 변화와 밝기센서

--- sketch_mar17c.ino
+++ sketch_mar17c.ino
@@ -1,14 +1,30 @@
+int pinNo[3] = {4,6,8};
+int state =0;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(10,INPUT);
   pinMode(6,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(4,OUTPUT);
 }
 
 void loop() {
   int Value = analogRead(0);
+  int pinValue = digitalRead(10);
   Serial.println(Value);
   delay(200);
+
+  if(pinValue == 0)
+  {
+    state = (state+1)%3;
+  }
+  
   if(Value >700)
-  digitalWrite(6,HIGH);
+  digitalWrite(pinNo[state],HIGH);
   else
-  digitalWrite(6,LOW);
+  digitalWrite(pinNo[state],LOW);
+  digitalWrite(pinNo[(state+1)%3],LOW);
+  digitalWrite(pinNo[(state+2)%3],LOW);
 }
+
