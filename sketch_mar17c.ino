752d79c8d7c62a252a38dcf8536caad9b7f7fec0
SocWoo <kwkps21@gmail.com>
2016-03-17 12:07:43 +0900

    밝기센서,가변저항

---
+++ sketch_mar17c.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(6,OUTPUT);
+}
+
+void loop() {
+  int Value = analogRead(0);
+  Serial.println(Value);
+  delay(200);
+  if(Value >700)
+  digitalWrite(6,HIGH);
+  else
+  digitalWrite(6,LOW);
+}
