ef3abc09282bca8931f0476939ea69fa6f8b137e
SocWoo <kwkps21@gmail.com>
2016-03-17 23:27:39 -0700

    교수님 코딩

--- sketch_mar18b.ino
+++ sketch_mar18b.ino
@@ -1,52 +1,39 @@
-int pinNo[4] = {4,5,6,7};
-int state = 0;
+int ledPin[4] = {4,5,6,7}; // LED(적황녹청);
+int pPin = 0;//가변저항
+int sPin = 9;//스위치
+int pState = 0;//LED배열의 인덱스(적녹황청)
+int sState = 0; //on,off
 
 void setup() {
   Serial.begin(9600);
-  pinMode(4,OUTPUT);//적
-  pinMode(5,OUTPUT);//황
-  pinMode(6,OUTPUT);//초
-  pinMode(7,OUTPUT);//파
-  pinMode(8,INPUT);//스위치
+  for(int i =0;i<4;i++)
+  {
+    pinMode(ledPin[i],OUTPUT);
+  }
+  pinMode(sPin , INPUT);
 }
 
 void loop() {
-  int Res = analogRead(0);
-  int Swi = digitalRead(8);
-  Serial.println(Res);
+  int sValue = digitalRead(sPin);
+  Serial.println(sValue);
+  if(sValue == 0){
+    if(sState == 0) sState = 1;
+    else if(sState == 1) sState = 0;
+  }
+
+  int pValue = analogRead(pPin);
+  Serial.println(pValue);
+  if(pValue<300) pState = 0;
+  else if(pValue<600) pState = 1;
+  else if(pValue <900) pState = 2;
+  else pState = 3;
+
+  for(int i = 0;i<4;i++){
+    digitalWrite(ledPin[i],LOW);
+  }
+
+  if(sState == 1) digitalWrite(ledPin[pState],HIGH);
+
   delay(200);
-  if(Swi == 0)
-  {
-    state = (state+1)%2;
-  }
-  if(state == 1)
-  {
-   if(Res <= 250)
-    {
-      digitalWrite(4,HIGH);
-      digitalWrite(5,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(7,LOW);
-    }
-    else if(Res <= 500)
-    {
-      digitalWrite(5,HIGH);
-      digitalWrite(6,LOW);
-      digitalWrite(7,LOW);
-    }
-    else if(Res <= 750)
-    {
-      digitalWrite(6,HIGH);
-      digitalWrite(7,LOW);
-   }
-    else
-      digitalWrite(7,HIGH);
-  }
-   else
-    {
-      digitalWrite(4,LOW);
-      digitalWrite(5,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(7,LOW);
-    }
+
 }
