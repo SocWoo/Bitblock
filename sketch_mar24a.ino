d47f8051a3849d89465c9599a713d4e39f79d0da
SocWoo <kwkps21@gmail.com>
2016-03-24 12:49:48 +0900

    부저 스위치

---
+++ sketch_mar24a.ino
@@ -0,0 +1,34 @@
+int piazzoPin = 4;
+int switchPin = 5;
+int sound[] = {261,294,330,349,392,440,494,542,590,630};
+int index = 0;
+int count = 10;
+int state = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(piazzoPin, OUTPUT);
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  int swi = digitalRead(switchPin);
+  Serial.println(swi);
+
+  if(swi == LOW)
+  {
+    state = (state+1)%2;
+  }
+  if(state == 1)
+  {
+      tone(piazzoPin,sound[index],500);
+      index = (index+1)%10;
+  }
+  delay(200);
+}
+
+
+
+
+
+
