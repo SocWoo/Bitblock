aafdae7056eb8466950c8f016f27576ae09b0cb3
SocWoo <kwkps21@gmail.com>
2016-03-25 15:40:53 +0900

    1스위치 누르면 모터 돌아기기

---
+++ sketch_mar25d.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+int servoPin = 6;
+int switchPin = 5;
+int pPin = 0;
+
+Servo moter;
+
+void setup() {
+  moter.attach(servoPin);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+
+  int swi = digitalRead(switchPin);
+  Serial.println(swi);
+
+  if(swi == LOW)
+  {
+      moter.write(180);
+      delay(100);
+  }
+  else
+  {
+    moter.write(0);
+    delay(100);
+  }
+
+}
