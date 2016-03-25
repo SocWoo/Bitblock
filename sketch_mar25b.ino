d1f9833e0e8b5e240b6f4c4639ca1d7b77c9537d
SocWoo <kwkps21@gmail.com>
2016-03-25 15:14:40 +0900

    가변저항에 따른 음계설정 (과제)

---
+++ sketch_mar25b.ino
@@ -0,0 +1,18 @@
+int pPin = 0;
+int buzzerPin = 4;
+int sound[] = {512,587,659,699,740,784,880,988,1047,1175};
+int index = 0;
+
+void setup() {
+  pinMode(buzzerPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int bValue;
+  int pValue = analogRead(pPin);
+  int pValue2 = map(pValue,0,1023,20,4000);
+  Serial.println(pValue2);
+
+    tone(buzzerPin,pValue,500);
+}
