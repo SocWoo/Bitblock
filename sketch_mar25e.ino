896275e04fe7507642c6fd202999cc4f34a56edf
SocWoo <kwkps21@gmail.com>
2016-03-25 15:41:33 +0900

    스위치 한번누르면 오른쪽 다시 누르면 왼쪽 돌아가는 서보모터

---
+++ sketch_mar25e.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+int servoPin = 6;
+int switchPin = 5;
+int pPin = 0;
+int state = 0;
+
+Servo moter;
+
+void setup() {
+  moter.attach(servoPin);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+
+  int swi = digitalRead(switchPin);
+  Serial.println(swi);
+
+  if(swi == LOW)
+  {
+    state = (state+1)%2;
+  }
+  if(state == 1)
+  {
+    moter.write(180);
+    delay(100);
+  }
+  else if(state == 0)
+  {
+    moter.write(0);
+    delay(100);
+  }
+  delay(200);
+
+}
