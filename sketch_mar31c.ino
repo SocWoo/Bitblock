748bffc62826d6b6b5998188d55cc156002a79d2
SocWoo <kwkps21@gmail.com>
2016-03-31 12:44:57 +0900

    세그먼트

---
+++ sketch_mar31c.ino
@@ -0,0 +1,40 @@
+int findPin[4]={8,9,10,11};
+byte findData[10][4] = {{0,0,0,0},
+                        {0,0,0,1},
+                        {0,0,1,0},
+                        {0,0,1,1},
+                        {0,1,0,0},
+                        {0,1,0,1},
+                        {0,1,1,0},
+                        {0,1,1,1},
+                        {1,0,0,0},
+                        {1,0,0,1}};
+                        
+void setup() {
+  
+  for(int i=0;i<4;i++)
+  {
+    pinMode(findPin[i],OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  for(int i=0; i<10;i++)
+  {
+//    for(int j = 0;j<4;j++)
+//    {
+//      digitalWrite(findPin[j],findData[num][j]);
+//    }
+    displayNum(i);
+    delay(500);
+  }
+}
+
+void displayNum(int num){
+  for(int j = 0;j<4;j++)
+  {
+    digitalWrite(findPin[j],findData[num][j]);
+  }
+}
+
