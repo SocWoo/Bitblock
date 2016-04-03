9ecf93b3750a366b0f0368924583f62a2601a1ba
SocWoo <kwkps21@gmail.com>
2016-04-03 19:08:02 +0900

    5주 과제
    1. 피에조 부저를 이용하여 10cm 이내의 경우 경고음을 내는 후방 탐지기 구현하시오.
    
    
        단, 스위치를 활용하여 후방 탐지기 동작 유무를 선택할 수 있게 할 것(수업시간 실습내용).
    
    
    2. 1) 문제에서 장애물 접근 거리(10cm이내)에 따라 경고음의 간격이 달라지도록 구현하시오.
        (힌트) 접근거리에 따라 tone()함수의 소리내는 시간과 delay()함수의 기다리는 시간을 조절
    
    
    3. 1) 문제에서 10cm 이내의 경우 부저음과 함께 FND에 거리를 숫자로 표시하시오.

---
+++ sketch_apr03a.ino
@@ -0,0 +1,76 @@
+int trigPin = 4;
+int ecoPin = 5;
+int swiPin = 6;
+int buzzerPin = 7;
+int findPin[4] = {8,9,10,11};
+byte findData[10][4] = {{0,0,0,0},
+                       {0,0,0,1},
+                       {0,0,1,0},
+                       {0,0,1,1},
+                       {0,1,0,0},
+                       {0,1,0,1},
+                       {0,1,1,0},
+                       {0,1,1,1},
+                       {1,0,0,0},
+                       {1,0,0,1}};
+int state = 0;
+
+void setup() {
+  pinMode(trigPin,OUTPUT);
+  pinMode(ecoPin,INPUT);
+  pinMode(swiPin,INPUT);
+  pinMode(buzzerPin,OUTPUT);
+  
+  for(int i = 0;i<4;i++)
+  {
+    pinMode(findPin[i],OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void displayNum(int num)
+{
+  for(int j = 0;j < 4; j++)
+  {
+    digitalWrite(findPin[j],findData[num][j]);
+  }
+}
+
+void loop() {
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  int swi = digitalRead(swiPin);
+  int receivedTime = pulseIn(ecoPin,HIGH);
+  float distance = receivedTime/58.2;
+
+  if(swi == 0)
+  {
+    state = (state+1)%2;
+  }
+  if(state == 1)
+  {
+    if(distance>=10 && distance<=30)
+    {
+      tone(buzzerPin,250,200);
+      delay(200);
+    }
+    for(int m = 0;m<10;m++)
+    {
+      if(m < distance && distance < m+1)
+      {
+        tone(buzzerPin,250,100 + (m*15));
+        displayNum(m);
+        delay(10 + m*10);
+      }
+    }
+  }
+
+  Serial.println(distance);
+  delay(200);
+}
+
+
+
+
+
